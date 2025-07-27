b6f6566e5760007c68a2cfb4e29cc964e5acf68e
felipetexa <felipe.teixeira91@hotmail.com>
2025-07-27 14:11:15 -0400

    initial work on new classes frontend

--- SmartGym/Models/DTOs/ClassDTO.cs
+++ SmartGym/Models/DTOs/ClassDTO.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SmartGym.Models;
 
 public class ClassDTO
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Name is required")]
     public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Schedule is required")]
     public DateTime Schedule { get; set; }
+
+    [Range(1, 100, ErrorMessage = "Capacity must be between 1 and 100")]
     public int Capacity { get; set; }
+
+    [Required(ErrorMessage = "Trainer ID is required")]
     public int TrainerId { get; set; }
     public int CategoryId { get; set; }
 }
