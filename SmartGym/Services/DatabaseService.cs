5cfeada87b0e6c2a5aa0426de7e182826aec7a7f
callmecelsius <celsius.mp@gmail.com>
2025-07-27 12:19:29 -0500

    removed using statement for dapper

--- SmartGym/Services/DatabaseService.cs
+++ SmartGym/Services/DatabaseService.cs
@@ -3,7 +3,6 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using Dapper;
 using Microsoft.EntityFrameworkCore;
 using SmartGym.Data;
 using SmartGym.Models;
