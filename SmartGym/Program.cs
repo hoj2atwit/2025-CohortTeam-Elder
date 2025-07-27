230a16733255046721c96e547dc3c22e15caf8c5
callmecelsius <celsius.mp@gmail.com>
2025-07-27 11:12:02 -0500

    First steps of connecting to db and running tests to insert

--- SmartGym/Program.cs
+++ SmartGym/Program.cs
@@ -5,6 +5,12 @@ using SmartGym.Services;
 using Microsoft.AspNetCore.Mvc;
 using SmartGym.Data;
 using Microsoft.EntityFrameworkCore;
+using SmartGym.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using SmartGym.Services;
+using Microsoft.AspNetCore.Mvc;
+using SmartGym.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,27 +27,37 @@ builder.Services.AddDbContext<SmartGymContext>(options =>
 	var connectionString = builder.Configuration.GetConnectionString("DBConnectionString");
 	options.UseSqlServer(connectionString);
 });
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<DatabaseConfiguration>(builder.Configuration.GetSection(DatabaseConfiguration.ConnectionStrings));
+builder.Services.AddTransient<IDatabaseService, DatabaseService>();
+builder.Services.AddDbContext<SmartGymContext>(options =>
+{ 
+	var connectionString = builder.Configuration.GetConnectionString("DBConnectionString");
+	options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
-
-#region Remove later, for testing only
 try
 {
 	using (var scope = app.Services.CreateScope())
 	{
 		var db = scope.ServiceProvider.GetRequiredService<IDatabaseService>();
-		// var db.CreateNewUser();
+		db.TestInsert();
 	}
 }
 catch (System.Exception ex)
 {
+
 	throw;
 }
-#endregion
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
@@ -55,6 +71,14 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
