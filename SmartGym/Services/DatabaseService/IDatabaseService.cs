2ac9e539df4d2d680d073bba868f9023ea4998b5
callmecelsius <celsius.mp@gmail.com>
2025-07-27 22:11:03 -0500

    deprecated DatabaseService. Leaving it in incase anyone wants to do something with this

--- SmartGym/Services/DatabaseService/IDatabaseService.cs
+++ SmartGym/Services/DatabaseService/IDatabaseService.cs
@@ -5,35 +5,5 @@ namespace SmartGym.Services;
 
 public interface IDatabaseService
 {
-	#region User
-
-	void CreateUser(User user);
-	void UpdateUser(User user);
-	void GetUser(User user);
-	void GetUserCheckInHistory(User user);
-	void GetAllUsers();
-	void GetUserPaymentMethod(User user);
-	void GetTrafficData(DateTime date);
-	void GetTrafficData(DateTime startDate, DateTime endDate);
-
-	#endregion
-
-
-	#region Cafe
-
-	void GetOrderHistory(User user);
-	void GetFullMenu();
-	void GetMealPrepItems();
-	void GetCafeItemModifications();
-	void GetCurrentPromos();
-
-	#endregion
-
-	#region Classes
-	Task<bool> CreateClass(ClassDTO newClassData);
-
-
-	#endregion
-
 
 }
