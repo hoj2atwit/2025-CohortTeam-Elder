2dcc6da75604dee1d577dbd1011c6305043185b5
callmecelsius <celsius.mp@gmail.com>
2025-07-28 11:09:50 -0500

    Squashed commit of the following:
    commit 06e1960fd62fd8eb15ec98032b643c8050900fb7
    Author: kelvin nguyen <kelvin.devpoint@outlook.com>
    Date:   Mon Jul 28 13:21:49 2025 +0000
    
        Merged PR 30: Created Methods and foundation for db backend
        - New Feature
        - Database
        This pull request uses the Repository pattern and will be the template we will follow to pull and push to the database.
    
        Controllers: API Layer
        Services: Business Layer
        Repositories: Data Access
    
        Other changes:
        - Models have been split up into DTO and Entities for clarity.
        - Services have been split up into contexts (user/class/etc), with accompanying repositories
        Please Confirm the following have been addressed:
        - [X] My code follows the style guidelines of this project
        - [X] I have performed a self-review of my code
        - [X] I have commented my code, particularly in hard-to-understand areas
        - [X] My changes generate no new warnings
        - [X] Any dependent changes have been merged and published in downstream modules
        Closed: #113
    
        - Removing app settings from vc
        - Adding ignore entry for appsettings dev
        - Squashed commit of the following:
        - First steps of connecting to db and running tests to insert
        - tested and confirmed database can be connected to and queries can be run via EF
        - Squashed commit of the following:
        - Removing app settings from vc
        - Adding ignore entry for appsettings dev
        - First steps of connecting to db and running tests to insert
        - tested and confirmed database can be connected to and queries can be run via EF
        - Merge branch 'DbSchema/Infrastructure/111-Connect-To-Db' of https://dev.azure.com/DSD-Cohort/Smart%20Gym/_git/Smart%20Gym into DbSchema/Infrastructure/111-Connect-To-Db
        - removing dapper
        - removed using statement for dapper
        - - started a few database methods for Users and Cafe
        - refactered code to follow repository pattern
        - deprecated DatabaseService. Leaving it in incase anyone wants to do something with this
    
        Related work items: #113

--- SmartGym/Program.cs
+++ SmartGym/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using SmartGym.Data;
 using SmartGym.Models;
 using Microsoft.EntityFrameworkCore;
+using SmartGym.Services.UserService;
 using Microsoft.EntityFrameworkCore.Design;
 using SmartGym.Services.UserService;
 
