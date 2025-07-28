cc7d39cb8e9110664ea8747dc79bb6b057970bd9
callmecelsius <celsius.mp@gmail.com>
2025-07-27 23:09:34 -0500

    Refactored to follow repository pattern and created the foundation for creating methods for data access

---
+++ SmartGym/Data/Repositories/UserRepo.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using SmartGym.Models;
+
+namespace SmartGym.Data;
+
+public class UserRepo : Repository<User>
+{
+	private readonly SmartGymContext _context;
+	private readonly DbSet<Class> _dbSet;
+
+	public UserRepo(SmartGymContext context) : base(context)
+	{
+		_context = context;
+		_dbSet = context.Set<Class>();
+	}
+	
+	#region Custom Methods
+
+	#endregion
+}
--- SmartGym/Services/DatabaseService/IDatabaseConfiguration.cs
+++ SmartGym/Services/DatabaseService/IDatabaseConfiguration.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SmartGym.Services.Interfaces
+namespace SmartGym.Services
 {
     public interface IDatabaseConfiguration
     {
