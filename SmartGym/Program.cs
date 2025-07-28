f906a8dc54d873b32575b230a586030eeae23476
callmecelsius <celsius.mp@gmail.com>
2025-07-27 23:15:47 -0500

    Squashed commit of the following:
    commit 5fe1dea9bd408adfc764ca09033cde1cd007a0ba
    Author: kelvin nguyen <kelvin.devpoint@outlook.com>
    Date:   Sun Jul 27 18:06:34 2025 +0000
    
        Merged PR 28: Connected solution to database.
        - [x] New Feature
        - [ ] Enhancement Feature
        - [x] Database Object
        - [ ] Bug
        - [ ] Documentation
        - [ ] Other
        - Removed app settings from vc
        - Adding ignore entry for appsettings for development
        - First steps of connecting to db and running tests to insert
        - tested and confirmed database can be connected to and queries can be run via EF
        ![Screenshot 2025-07-27 at 11.00.52 AM (2).png](blob:https://dev.azure.com/07b2f9a1-8e15-4f0c-b3c2-27333e8dd352)
        ![Screenshot 2025-07-27 at 11.01.36 AM.png](https://dev.azure.com/DSD-Cohort/627f4178-049d-458a-9509-73415b34f4ac/_apis/git/repositories/1a6ba7a3-8182-4544-9fc7-461addaecad6/pullRequests/28/attachments/Screenshot%202025-07-27%20at%2011.01.36%E2%80%AFAM.png)
        Please Confirm the following have been addressed:
        - [X] My code follows the style guidelines of this project
        - [ ] I have performed a self-review of my code
        - [X] I have commented my code, particularly in hard-to-understand areas
        - [ ] I have made corresponding changes to the documentation
        - [ ] My changes generate no new warnings
        Closed: #111
    
        Related work items: #111

--- SmartGym/Program.cs
+++ SmartGym/Program.cs
@@ -39,6 +39,9 @@ if (!app.Environment.IsDevelopment())
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -49,6 +52,14 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
--- SmartGym/Services/DatabaseService/IDatabaseService.cs
+++ SmartGym/Services/DatabaseService/IDatabaseService.cs
@@ -5,5 +5,4 @@ namespace SmartGym.Services;
 
 public interface IDatabaseService
 {
-
 }
