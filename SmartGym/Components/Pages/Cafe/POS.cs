510dc02500775b929fca3bd1d41d0048e743a585
hoj2atwit <johnniezho@gmail.com>
2025-07-30 02:00:13 -0400

    Added Search Functionality

--- SmartGym/Components/Pages/Cafe/POS.cs
+++ SmartGym/Components/Pages/Cafe/POS.cs
@@ -7,7 +7,8 @@ namespace SmartGym.Components.Pages.Cafe
         public static POS instance = new POS();
 
         public MenuCartLayout.Cart? cart = new();
-        public List<MenuItemLayout.MenuItem> fullMenu = new(); 
+        public List<MenuItemLayout.MenuItem> fullMenuList = new(); 
+        private Dictionary<string, MenuItemLayout.MenuItem> fullMenuDict = new();
         public List<MenuItemLayout.MenuItem> filteredMenu = new();
         public POS() 
         {
@@ -21,8 +22,32 @@ namespace SmartGym.Components.Pages.Cafe
             }
 
             cart = new MenuCartLayout.Cart();
-            this.fullMenu = MenuItemLayout.generateItemList();
-            filteredMenu = this.fullMenu;
+            fullMenuList = MenuItemLayout.MenuItem.generateItemList();
+            fullMenuDict = MenuItemLayout.MenuItem.generateItemDict();
+            filteredMenu = fullMenuList;
         }
+
+
+
+        public void search(string search) 
+        {
+            if (search.Trim() == "" ) 
+            {
+                filteredMenu = fullMenuList;
+                return;
+            }
+            //Use search value to apply regex requirements to keys in menu or DB
+            List<MenuItemLayout.MenuItem> filteredList = new List<MenuItemLayout.MenuItem>();
+            foreach (string name in fullMenuDict.Keys) {
+                if (name.Contains(search))
+                {
+                    //Check for filter requirement
+                    filteredList.Add(fullMenuDict[name]);
+                }
+            }
+            
+            filteredMenu = filteredList;
+        }
+
     }
 }
