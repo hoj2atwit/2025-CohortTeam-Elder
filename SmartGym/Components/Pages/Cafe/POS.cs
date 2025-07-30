ebd41d2a91e38497096625565d066707ff0212ae
hoj2atwit <johnniezho@gmail.com>
2025-07-30 01:30:01 -0400

    Added dynamic Cart usage to POS - Cart is able to me added and removed from - Cart can be filled dynamically with items from the menu - Added Dynamic support within app.razor

---
+++ SmartGym/Components/Pages/Cafe/POS.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SmartGym.Components.Pages.Cafe
+{
+    public class POS
+    {
+        public static POS instance = new POS();
+
+        public MenuCartLayout.Cart? cart = new();
+        public List<MenuItemLayout.MenuItem> fullMenu = new(); 
+        public List<MenuItemLayout.MenuItem> filteredMenu = new();
+        public POS() 
+        {
+            if (instance != null)
+            {
+                instance = new POS();
+            }
+            else 
+            {
+                instance = this;
+            }
+
+            cart = new MenuCartLayout.Cart();
+            this.fullMenu = MenuItemLayout.generateItemList();
+            filteredMenu = this.fullMenu;
+        }
+    }
+}
