cdae0483d1fc7c5ff9477b28f000779a8c3c11b2
hoj2atwit <johnniezho@gmail.com>
2025-07-30 14:55:07 -0400

    Separated Cart and MenuItem into their own class files. - Moved MenuItem in to its own class to debulk code. - Moved Cart and CartItem together into their own class to debulk Code. - Added fix for Input Exception. - Made Cart an OrderedDictionary to try to solve unordered cart issue. Unsure if resolved. Requires more testing.

---
+++ SmartGym/Components/Pages/Cafe/Cart.cs
@@ -0,0 +1,80 @@
+﻿namespace SmartGym.Components.Pages.Cafe
+{
+    public class Cart
+    {
+        public OrderedDictionary<int, CartItem> cartItems = new();
+        private decimal total = 0;
+        public string totalString = "";
+
+        public Cart()
+        {
+            updateTotal();
+        }
+
+        public void removeItem(int id)
+        {
+            cartItems.Remove(id);
+            updateTotal();
+        }
+
+        public void addItem(MenuItem item)
+        {
+            if (cartItems.ContainsKey(item.itemId))
+            {
+                cartItems[item.itemId].increaseAmount();
+            }
+            else
+            {
+                cartItems.Add(item.itemId, new CartItem(item, 1));
+            }
+            updateTotal();
+        }
+
+        public void updateTotal()
+        {
+            total = 0;
+            foreach (CartItem ci in cartItems.Values)
+            {
+                total += ci.currentPrice;
+            }
+            totalString = String.Format("{0:C}", total);
+        }
+
+        public class CartItem
+        {
+            public MenuItem? item;
+            public int amount = 0;
+            public decimal currentPrice;
+            public string? currentPriceString;
+
+            public CartItem(MenuItem item, int amount)
+            {
+                this.item = item;
+                updatePrice(amount);
+            }
+
+            public void updatePrice(int amount)
+            {
+                this.amount = amount;
+                updatePrice();
+            }
+            public void updatePrice()
+            {
+                currentPrice = item.price * amount;
+                currentPriceString = String.Format("{0:C}", currentPrice);
+            }
+
+            public void increaseAmount()
+            {
+                amount++;
+                updatePrice();
+            }
+
+            public void reduceAmount()
+            {
+                amount--;
+                updatePrice();
+            }
+        }
+    }
+}
---
+++ SmartGym/Components/Pages/Cafe/MenuItem.cs
@@ -0,0 +1,67 @@
+﻿namespace SmartGym.Components.Pages.Cafe
+{
+    public class MenuItem
+    {
+        public int itemId = 0;
+        public decimal price = 0.0m;
+        public string name = "";
+        public string description = "";
+        public string priceString = "";
+        public string imageLocation = "";
+
+        public MenuItem()
+        {
+            setPrice(0);
+            name = "None";
+            setImageLocation("None");
+            itemId = 0;
+            description = "No Description";
+        }
+
+        public MenuItem(string name, decimal price, int itemId, string description)
+        {
+            setPrice(price);
+            this.name = name;
+            setImageLocation(name);
+            this.itemId = itemId;
+            this.description = description;
+        }
+        private void setImageLocation(string name)
+        {
+            //True Implementation of image location. Adapt to naming convention
+            //imageLocation = $"lib/images/{name}.jpg";
+            //Just using Coffee Image for now
+            imageLocation = $"lib/images/Coffee.jpg";
+        }
+
+        private void setPrice(decimal p)
+        {
+            price = p;
+            priceString = String.Format("{0:C}", p);
+        }
+
+        public static List<MenuItem> generateItemList()
+        {
+            List<MenuItem> menuItems = new List<MenuItem>();
+            //TODO: Have this generate a list of menu items from the database
+            for (int i = 0; i < 50; i++)
+            {
+                menuItems.Add(new MenuItem("Coffee " + i, 6.99m, i, "Cup of Coffee"));
+            }
+
+            return menuItems;
+        }
+
+        public static Dictionary<string, MenuItem> generateItemDict()
+        {
+            Dictionary<string, MenuItem> menuItems = new Dictionary<string, MenuItem>();
+            //TODO: Have this generate a list of menu items from the database
+            for (int i = 0; i < 50; i++)
+            {
+                menuItems.Add("Coffee " + i, new MenuItem("Coffee " + i, 6.99m, i, "Cup of Coffee"));
+            }
+
+            return menuItems;
+        }
+    }
+}
