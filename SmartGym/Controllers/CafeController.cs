5816beabaa15cd21ccf2e3ce38c774f847ad6140
callmecelsius <celsius.mp@gmail.com>
2025-08-03 17:22:11 -0500

    revert code to before I picked up Menu items

--- SmartGym/Controllers/CafeController.cs
+++ SmartGym/Controllers/CafeController.cs
@@ -16,7 +16,6 @@ public class CafeController : ControllerBase
 		_cafeService = cafeService;
 	}
 
-	#region Menu Items
 	[HttpGet("menu")]
 	public async Task<ActionResult<IEnumerable<Order>>> GetFullMenu()
 	{
@@ -67,76 +66,4 @@ public class CafeController : ControllerBase
 	// {
 	// 	throw new NotImplementedException();
 	// }
-	#endregion
-
-	#region Orders
-	/// <summary>
-	/// Get entire user order history
-	/// </summary>
-	/// <param name="id"></param>
-	/// <returns></returns>
-	[HttpGet("orders/user/{id:int}")]
-	public async Task<ActionResult<IEnumerable<Order>>> GetAllUserOrders(int id)
-	{
-		var orderList = await _cafeService.GetAllUserOrders(id);
-		return Ok(orderList);
-	}
-	/// <summary>
-	/// Get order by user ID
-	/// </summary>
-	/// <param name="id"></param>
-	/// <returns></returns>
-	[HttpGet("orders/{id:int}")]
-	public async Task<ActionResult<OrderDTO>> GetOrderById(int id)
-	{
-		var orderItem = await _cafeService.GetOrderById(id);
-		if (orderItem == null)
-		{
-			return NotFound();
-		}
-		return orderItem;
-	}
-	/// <summary>
-	/// Get order's time by order Id
-	/// </summary>
-	/// <param name="id"></param>
-	/// <returns></returns>
-	[HttpGet("orders/{id:int}/time")]
-	public async Task<ActionResult<DateTime?>> GetOrderTime(int id)
-	{
-		var orderTime = await _cafeService.GetOrderTime(id);
-		return orderTime;
-	}
-	/// <summary>
-	/// Create a new order
-	/// </summary>
-	/// <param name="newOrderData"></param>
-	/// <returns></returns>
-	[HttpPost("orders/create")]
-	public async Task<ActionResult<OrderDTO>> CreateOrder(OrderDTO newOrderData)
-	{
-		var created = await _cafeService.CreateOrder(newOrderData);
-		return CreatedAtAction(nameof(GetOrderById), new { id = created.Id }, created);
-	}
-
-	[HttpPatch("orders/{id:int}")]
-	public async Task<ActionResult<OrderDTO>> UpdateOrder(int id, [FromBody] OrderPatchDTO orderToPatch)
-	{
-		var patchedOrder = await _cafeService.UpdateOrder(id, orderToPatch);
-		if (patchedOrder == null)
-		{
-			return NotFound();
-		}
-		return Ok(patchedOrder);
-	}
-
-	[HttpDelete("orders/{id:int}")]
-	public async Task<IActionResult> DeleteOrder(int id)
-	{
-		var removed = await _cafeService.DeleteOrder(id);
-		if (!removed)
-			return NotFound();
-		return Ok();
-	}
-	#endregion
 }
--- SmartGym/Controllers/OrdersController.cs
+++ SmartGym/Controllers/OrdersController.cs
@@ -8,60 +8,60 @@ namespace SmartGym.Controllers;
 [Route("api/[controller]")]
 public class OrdersController : ControllerBase
 {
-//   private readonly IOrderService _service;
+  private readonly IOrderService _service;
 
-//   public OrdersController(IOrderService service)
-//   {
-//     _service = service;
-//   }
+  public OrdersController(IOrderService service)
+  {
+    _service = service;
+  }
 
-//   [HttpGet("user/{userId:int}")]
-//   public async Task<ActionResult<IEnumerable<Order>>> GetAllUserOrders(int userId)
-//   {
-//     var orderList = await _service.GetAllUserOrders(userId);
-//     return Ok(orderList);
-//   }
-//   [HttpGet("{id:int}")]
-//   public async Task<ActionResult<OrderDTO>> GetOrderById(int id)
-//   {
-//     var orderItem = await _service.GetOrderById(id);
-//     if (orderItem == null)
-//     {
-//       return NotFound();
-//     }
-//     return orderItem;
-//   }
-//   [HttpGet("{id:int}/time")]
-//   public async Task<ActionResult<DateTime?>> GetOrderTime(int id)
-//   {
-//     var orderTime = await _service.GetOrderTime(id);
-//     return orderTime;
-//   }
+  [HttpGet("user/{id:int}")]
+  public async Task<ActionResult<IEnumerable<Order>>> GetAllUserOrders(int id)
+  {
+    var orderList = await _service.GetAllUserOrders(id);
+    return Ok(orderList);
+  }
+  [HttpGet("{id:int}")]
+  public async Task<ActionResult<OrderDTO>> GetOrderById(int id)
+  {
+    var orderItem = await _service.GetOrderById(id);
+    if (orderItem == null)
+    {
+      return NotFound();
+    }
+    return orderItem;
+  }
+  [HttpGet("{id:int}/time")]
+  public async Task<ActionResult<DateTime?>> GetOrderTime(int id)
+  {
+    var orderTime = await _service.GetOrderTime(id);
+    return orderTime;
+  }
 
-//   [HttpPost]
-//   public async Task<ActionResult<OrderDTO>> CreateOrder(OrderDTO newOrderData)
-//   {
-//     var created = await _service.CreateOrder(newOrderData);
-//     return CreatedAtAction(nameof(GetOrderById), new { id = created.Id }, created);
-//   }
+  [HttpPost]
+  public async Task<ActionResult<OrderDTO>> CreateOrder(OrderDTO newOrderData)
+  {
+    var created = await _service.CreateOrder(newOrderData);
+    return CreatedAtAction(nameof(GetOrderById), new { id = created.Id }, created);
+  }
 
-//   [HttpPatch("{id:int}")]
-//   public async Task<ActionResult<OrderDTO>> UpdateOrder(int id, [FromBody] OrderPatchDTO orderToPatch)
-//   {
-//     var patchedOrder = await _service.UpdateOrder(id, orderToPatch);
-//     if (patchedOrder == null)
-//     {
-//       return NotFound();
-//     }
-//     return Ok(patchedOrder);
-//   }
+  [HttpPatch("{id:int}")]
+  public async Task<ActionResult<OrderDTO>> UpdateOrder(int id, [FromBody] OrderPatchDTO orderToPatch)
+  {
+    var patchedOrder = await _service.UpdateOrder(id, orderToPatch);
+    if (patchedOrder == null)
+    {
+      return NotFound();
+    }
+    return Ok(patchedOrder);
+  }
 
-//   [HttpDelete("{id:int}")]
-//   public async Task<IActionResult> DeleteOrder(int id)
-//   {
-//     var removed = await _service.DeleteOrder(id);
-//     if (!removed)
-//       return NotFound();
-//     return Ok();
-//   }
+  [HttpDelete("{id:int}")]
+  public async Task<IActionResult> DeleteOrder(int id)
+  {
+    var removed = await _service.DeleteOrder(id);
+    if (!removed)
+      return NotFound();
+    return Ok();
+  }
 }
