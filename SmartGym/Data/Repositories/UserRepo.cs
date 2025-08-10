b65a091afe016125ba21737ce82451e66f421d94
callmecelsius <celsius.mp@gmail.com>
2025-08-09 22:08:05 -0500

    added documentation

--- SmartGym/Data/Repositories/UserRepo.cs
+++ SmartGym/Data/Repositories/UserRepo.cs
@@ -17,6 +17,14 @@ public class UserRepo : Repository<AppUser>
 
 	#region Custom Methods
 
+	/// <summary>
+	/// Asynchronously retrieves all users from the database along with their associated roles,
+	/// and maps them to a list of <see cref="UserDto"/> objects.
+	/// </summary>
+	/// <returns>
+	/// A task that represents the asynchronous operation. The task result contains a list of <see cref="UserDto"/> 
+	/// objects representing all users and their roles.
+	/// </returns>
 	public async Task<List<UserDto>> GetAllAspUsersAsDto()
 	{
 		var usersWithRoles = await(
